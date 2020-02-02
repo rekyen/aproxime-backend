ea967fe0096e43963c64a6955a3feca9718fa151
Marco Lima <marco.lima.id+1@gmail.com>
2020-02-02 18:07:48 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // docs.sequelizejs.com - dialects
+  dialects: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'aproximepw',
+  database: 'aproxime',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
